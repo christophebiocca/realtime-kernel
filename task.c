36ea56edb87c937ca1d08a00ab89cf19f02a2c37
Christophe Biocca <christophe.biocca@gmail.com>
2012-05-17 19:09:07 -0400

    Use default stack size for initial task.

--- task.c
+++ task.c
@@ -43,7 +43,7 @@ void initTaskSystem(void) {
     // Priority 0 because init task must run to completion before anything else
     // it may even issue multiple syscalls and must be guaranteed to run after
     // them.
-    int ret = createTask(0, &initialTask, 1024, -1);
+    int ret = createTask(0, &initialTask, DEFAULT_STACK_SIZE, -1);
     if(ret < 0){
         bwprintf(COM2, "Fatal error, %d when setting up initial task");
     }
