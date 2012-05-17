39909888a69078cbb6d787745d74bb659c48df7b
Christophe Biocca <christophe.biocca@gmail.com>
2012-05-17 01:45:12 -0400

    Got the kernel to work.

--- kernel.c
+++ kernel.c
@@ -6,7 +6,7 @@
 struct TaskDescriptor {
     unsigned int ret;
     unsigned int spsr;
-    unsigned int sp;
+    unsigned int *sp;
 };
 
 struct Request {
@@ -17,24 +17,28 @@ static struct TaskDescriptor desc;
 static void initTask(struct TaskDescriptor *task){
     // By default, 1 process owning 0x250000 to 0x300000
     // with stack at 0x300000
+
+    // Stored sp points to bottom of stack
+    // Stack storage
+    // sp - 1 = r15 (pc)
+    // sp + 0 = r0
+    // ...
+    // sp + 12 = r12
+    // sp + 13 = r14 (lr)
+
     task->ret = 1;
     task->spsr = UserMode | DisableIRQ | DisableFIQ;
-    /* 14 because we don't pop SP and PC off the stack */
-    task->sp = 0x300000 - (14 * 4);
+    task->sp = ((unsigned int *) 0x300000) - 14;
 
-    for(int i = 0; i < 16; ++i){
-        *(((unsigned int *) task->sp) + i + 1) = 0;
+    for(int i = -1; i < 14; ++i){
+        *(task->sp + i) = i;
     }
-    *(((unsigned int *) task->sp) + 0) =
-        ((unsigned int) &userModeTask) + 0x200000;
-    *(((unsigned int *) task->sp) + 13) = 0x300000;
-    *(((unsigned int *) task->sp) + 11) = 0x300000;
+    *(task->sp - 1) = ((unsigned int) &userModeTask) + 0x200000; // Set starting pc
+    *(task->sp + 11) = 0x300000; // For now, set frame to same as sp
     bwprintf(COM2, "%x %x %x\r\n", task->ret, task->spsr, task->sp);
     bwputstr(COM2, "Stack:\r\n");
-
-    for(int i = 0; i < 16; ++i){
-        bwprintf(COM2, "\t%x(%x): %x\r\n", task->sp+i, i,
-        *(((unsigned int *) task->sp) + i));
+    for(int i = -1; i < 14; ++i){
+        bwprintf(COM2, "\t%x(sp+%x): %x\r\n", task->sp+i, i, *(task->sp+i));
     }
 }
 
@@ -59,41 +63,44 @@ int main(){
     for(unsigned int i = 0; i < 3; ++i){
         struct TaskDescriptor* active = &desc;
         bwputstr(COM2, "kerxitEntry\r\n");
+        
+        {
+            bwprintf(COM2, "%x %x %x\r\n", active->ret, active->spsr, active->sp);
+            bwputstr(COM2, "Stack:\r\n");
+            for(int i = -1; i < 14; ++i){
+                bwprintf(COM2, "\t%x(sp+%x): %x\r\n", active->sp+i, i, *(active->sp+i));
+            }
+        }
 
-        register unsigned int sp asm("r1") = active->sp;
-        register unsigned int spsr asm("r2") = active->spsr;
-        register unsigned int ret asm("r3") = active->ret;
-
-        asm volatile(
-            "stmfd sp!, {r0-r12, r14}\n\t"  // save kregs on kstack
-            "msr cpsr_flg, #0x1f\n\t"       // switch to system mode
-            "mov sp, %0\n\t"                // restore ustack pointer
-            "ldmfd sp!, {r0-r12, r14}\n\t"  // unroll uregs from ustack, except SP and PC
-            "mov lr, r0\n\t"                // reload user PC from r0
-            "mov r0, %2\n\t"                // load return value in r0
-            "msr cpsr_flg, #0x13\n\t"       // switch to supervisor mode
-            "msr spsr_all, %1\n\t"          // restore the spsr
-            "movs pc, lr\n\t"               // jump to user code!
-
-            : /* no output */
-            : "r"(sp), "r"(spsr), "r" (ret)
-        );
+        register unsigned int *sp  = active->sp;
+        register unsigned int spsr = active->spsr;
+        *sp = active->ret;
 
         asm volatile(
+            "stmfd sp!, {r6-r12, r14}\n\t"  // save kregs on kstack
+            "ldr r14, [%0, #-4]\n\t"        // Get the stored pc
+            "msr spsr, %1\n\t"              // set active's spsr
+            "msr cpsr_c, #0xdf\n\t"         // Switch to system mode
+            "mov sp, %0\n\t"                // Set sp.
+            "ldmfd sp!, {r0-r12, r14}\n\t"  // Unroll task registers.
+            "msr cpsr_c, #0xd3\n\t"         // Switch to supervisor mode
+            "movs pc, r14\n\t"              // JUMP!
             "kerlabel:\n\t"                 // ucode: SWI n
-            // TODO: acquire arguments to request here
-            "mov r0, lr\n\t"                // save the user PC in r0
-            "msr cpsr_flg, #0x1f\n\t"       // switch to system mode
-            "stmfd sp!, {r0-r12, r14}\n\t"  // store uregs on ustack
-            "mov r0, sp\n\t"                // move updated ustack pointer on r0
-            "msr cpsr_flg, #0x13\n\t"       // switch to supervisor mode
-            "ldmfd sp!, {r0-r12, r14}\n\t"  // unroll kregs from kstack
-            "mov %0, r0\n\t"                // store ustack pointer
-            "mrs %1, spsr_all\n\t"          // store the spsr
-
-            : "=r"(sp), "=r"(spsr)
+            "msr cpsr_c, #0xdf\n\t"         // Switch to system mode
+            "stmfd sp!, {r0-r12, r14}\n\t"  // Store user registers.
+            "mov %0, sp\n\t"                // Save user's stack pointer
+            "msr cpsr_c, #0xd3\n\t"         // Switch to supervisor mode
+            "str r14, [%0, #-4]\n\t"        // Store the task's pc on it's stack
+            "mrs %1, spsr\n\t"              // Obtain activity's spsr
+            "ldmfd sp!, {r6-r12, r14}\n\t"  // unroll kregs from kstack
+            : "+r"(sp), "+r"(spsr)
+            :
+            : "r0", "r1", "r2", "r3"
         );
 
+        active->sp = sp;
+        active->spsr = spsr;
+        active->ret = 0;
         bwputstr(COM2, "kerxitExit\r\n");
 
         struct Request req;
