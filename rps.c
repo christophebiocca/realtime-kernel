3210ed6c51b6c92d83afe56d0047fd9b96cf249c
Christophe Biocca <christophe.biocca@gmail.com>
2012-05-27 23:38:20 -0400

    Add player code.

--- rps.c
+++ rps.c
@@ -36,10 +36,10 @@ static void rpsServer(void) {
         state[i].tid = 0;
     }
 
-    LOG("Reticulating splines...");
+    LOG("Reticulating splines...\r\n");
     int ret = RegisterAs("rps");
 
-    if (!ret) {
+    if (ret < 0) {
         LOG("Error registering with name server: %d\r\n", ret);
         Exit();
     }
@@ -173,10 +173,70 @@ static void rpsServer(void) {
     Exit();
 }
 
+static unsigned int seed;
+
+static inline unsigned int next(int width){
+    seed = (seed * 1664525U + 1013904223U);
+    return seed >> (32-width);
+}
+
+static void rpsClient(void){
+    int server = WhoIs("rps");
+    if(server < 0){
+        bwprintf(COM2, "Got a bad response when doing name query! %d", server);
+        Exit();
+    }
+    int request;
+    int response;
+    int len;
+    for(int i = next(10); i >= 0;){
+        // Let's sign up
+        request = SIGN_UP;
+        len = Send(server, (char*) &request, 4, (char*) &response, 4);
+        if(len < 0){
+            bwprintf(COM2, "Got a bad response when signing up! %d", len);
+        }
+        for(; i >= 0; --i){
+            // Now let's play for some time.
+            int play;
+            while((play = next(2)) == 4); // To eliminate bias in picking a game.
+            rpsRequest moves[3] = {PLAY_ROCK, PLAY_PAPER, PLAY_SCISSORS};
+            request = moves[play];
+            len = Send(server, (char*) &request, 4, (char*) &response, 4);
+            if(len < 0){
+                bwprintf(COM2, "Got a bad response when playing! %d", len);
+                Exit();
+            }
+            if(response == OPPONENT_QUIT){
+                // We need to sign up again.
+                break;
+            }
+        }
+    }
+    request = QUIT;
+    len = Send(server, (char*) &request, 4, (char*) &response, 4);
+    if(len < 0){
+        bwprintf(COM2, "Got a bad response when quitting! %d", len);
+    }
+    Exit();
+}
+
+static void playerSpawner(void){
+    Create(2, rpsClient);
+    Create(2, rpsClient);
+    Exit();
+    while(1){
+        bwputstr(COM2, "No players are active at the moment, let's create one.\r\n");
+        if(Create(2, rpsClient) < 0){
+            bwputstr(COM2, "Couldn't create player, quitting!\r\n");
+            break;
+        }
+    }
+}
+
 void rpsUserModeTask(void) {
     Create(1, task_nameserver);
     Create(2, rpsServer);
-    //Create(2, rpsClient);
-    //Create(2, rpsClient);
+    Create(3, playerSpawner);
     Exit();
 }
