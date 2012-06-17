09def0403fc934ac338b65ac070735c9e971e857
Ronuk Raval <ronuk.raval@gmail.com>
2012-06-17 01:51:29 -0400

    user test task for train IO

--- user/init.c
+++ user/init.c
@@ -7,6 +7,7 @@
 
 #include <user/string.h>
 #include <user/mio.h>
+#include <user/tio.h>
 #include <user/priorities.h>
 
 static void childTask() {
@@ -163,8 +164,50 @@ void echoTask(void) {
     Exit();
 }
 
+/* Train IO test */
+void trainTask(void) {
+    mioInit();
+    tioInit();
+
+    struct String str;
+    sinit(&str);
+    sputstr(&str, "Hello World: ");
+    sputuint(&str, 123, 10);
+    sputstr(&str, "\r\n");
+    mioPrint(&str);
+
+    struct String out;
+    sinit(&out);
+    sputc(&out, 133);
+    tioPrint(&out);
+    
+    int i = 0;
+    while (1) {
+        struct String in;
+        tioRead(&in);
+
+        sinit(&str);
+        char *buf = sbuffer(&in);
+        for (unsigned int j = 0; j < slen(&in); ++j) {
+            sputuint(&str, buf[j], 16);
+        }
+        mioPrint(&str);
+
+        i = (i + slen(&in)) % 10;
+        if (i == 0) {
+            sinit(&str);
+            sputstr(&str, "\r\n");
+            mioPrint(&str);
+
+            tioPrint(&out);
+        }
+    }
+
+    Exit();
+}
+
 void echoTaskInit(void) {
-    Create(TASK_PRIORITY, echoTask);
+    Create(TASK_PRIORITY, trainTask);
     Create(31, idlerTask);
     Exit();
 }
