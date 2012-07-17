881cc424d8ef8aae5229ff7dfd8eca9b9a4bf34e
Christophe Biocca <christophe.biocca@gmail.com>
2012-07-17 08:08:45 -0400

    Avoid reversing.

--- user/pathfinding.c
+++ user/pathfinding.c
@@ -70,7 +70,7 @@ int planPath(struct TrackNode *list, int train_id,
 
         if (next->node->type & ~(NODE_MERGE | NODE_BRANCH | NODE_BRANCH_STRAIGHT | NODE_BRANCH_CURVED)) {
             seq[seqCount] = next->node->reverse;
-            seqCost[seqCount++] = 0;
+            seqCost[seqCount++] = 1500;
         }
 
         if (next->node->type != NODE_EXIT) {
